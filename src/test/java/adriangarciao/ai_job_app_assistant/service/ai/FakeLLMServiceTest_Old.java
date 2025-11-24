ecbbb8240d044efa379c379bb97fd6f26056422d
adriangarciao <garcia.adriann08@gmail.com>
2025-11-24 17:50:03 -0600

    test: neutralize duplicate FakeLLMServiceTest placeholder

---
+++ src/test/java/adriangarciao/ai_job_app_assistant/service/ai/FakeLLMServiceTest_Old.java
@@ -0,0 +1,6 @@
+package adriangarciao.ai_job_app_assistant.service.ai._moved;
+
+/** Harmless placeholder to neutralize a previously duplicated test. */
+public final class FakeLLMServiceTest_Old {
+    private FakeLLMServiceTest_Old() { }
+}
